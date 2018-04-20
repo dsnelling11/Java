5fa6b48f52a929abe72e264625af3f6c6dca4a18
Darries <34078312+dsnelling11@users.noreply.github.com>
2018-04-19 23:25:38 -0400

    Run the domino game

---
+++ Domino Game/Main.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ * 
+ * @author Darries Snelling
+ * 
+ *         This is the main datatype that instantiates the domino object in the
+ *         domino class
+ *
+ */
+
+public class Program2 {
+	public static void main(String[] args) {
+		Game myGameTable; 
+	    myGameTable = new Game();
+
+	}
+
+}
