7b2ed685480cd6c7cca77401971770c01df4f8a3
Darries <34078312+dsnelling11@users.noreply.github.com>
2018-04-19 22:46:57 -0400

    main

---
+++ Domino Progam/Main.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+/**
+ * 
+ * @author Darries Snelling
+ * 
+ *         This is the main datatype that instantiates the domino object in the
+ *         domino class
+ *
+ */
+
+public class Program2 {
+	public static void main(String[] args) {
+		DominoPanel mainDomino = new DominoPanel();
+
+	}
+
+}
